8b5d76f87f11ca1c92daf378d45f5c354ea825d1
Sakar Aryal <techiesakar@gmail.com>
2023-05-27 10:45:29 +0545

    Collapse Button Logics Changed

--- src/navigation/Sidebar.jsx
+++ src/navigation/Sidebar.jsx
@@ -16,27 +16,41 @@ const Sidebar = () => {
   };
   return (
     <aside
-      className={`fixed -left-56 md:left-0 top-0 transition-all duration-300  bg-cyan-950 h-screen whitespace-nowrap  md:flex flex-col gap-6 justify-between py-6 ${
+      className={`sidebar fixed -left-56 md:left-0 top-0 transition-all duration-300  bg-cyan-950 h-screen whitespace-nowrap  md:flex flex-col gap-6 justify-between py-6 ${
         toggle ? "w-16" : "w-52"
       }`}
     >
       <div className="flex flex-col gap-6 px-4">
-        <Link to={"/"} className="flex gap-2 items-center">
-          <img src={Favicon} alt="SR Inventory" className="w-8 h-8" />
+        <div className="h-14 site-branding">
+          <Link to={"/"} className="flex gap-2 items-center">
+            <img
+              src={Favicon}
+              alt="SR Inventory"
+              className="w-8 h-8 site-logo"
+            />
 
-          {!toggle && (
-            <span className="flex flex-col items-start text-white leading-4">
-              <span className="font-bold ">SR</span>
-              <span>Inventory</span>
-            </span>
-          )}
-        </Link>
-        <ul className="flex flex-col gap-6">
+            {!toggle && (
+              <span className="flex flex-col items-start text-white leading-4">
+                <span className="font-bold ">SR</span>
+                <span>Inventory</span>
+              </span>
+            )}
+          </Link>
+        </div>
+        <ul className="flex flex-col gap-6 primary-menu">
           {NavLinks.map((item, index) => {
             return (
-              <li key={index} className="flex items-center text-white">
-                <Link to={item.link} className="flex items-center gap-3">
-                  <button className="text-2xl">{item.icon}</button>
+              <li
+                key={index}
+                className="flex items-center text-white menu-item"
+              >
+                <Link
+                  to={item.link}
+                  className="flex items-center gap-3 menu-link"
+                >
+                  <button className="text-2xl" title={item.title}>
+                    {item.icon}
+                  </button>
                   {!toggle && item.title}
                 </Link>
               </li>
@@ -45,18 +59,18 @@ const Sidebar = () => {
         </ul>
       </div>
 
-      <div className="text-white flex gap-3 items-center px-4">
+      <div className="text-white flex gap-3 items-center px-4 toggle-wrapper">
         <div className="text-2xl" onClick={handleToggle}>
-          {toggle ? (
-            <button className="flex items-center gap-3">
+          <button>
+            {toggle ? (
               <TbLayoutSidebarRightCollapse />
-            </button>
-          ) : (
-            <button className="flex items-center gap-3">
-              <TbLayoutSidebarLeftCollapse />
-              <span className="text-sm">Collapse</span>
-            </button>
-          )}
+            ) : (
+              <span className="flex items-center gap-3">
+                <TbLayoutSidebarLeftCollapse />
+                <span className="text-sm">Collapse</span>
+              </span>
+            )}
+          </button>
         </div>
       </div>
     </aside>
